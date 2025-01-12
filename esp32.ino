6b196e5f2c310e4e73b9cdef9f950f76afe0928a
Hebimalu0000 <hebimalu@outlook.com>
2025-01-12 12:45:03 +0900

    Create esp32.ino

---
+++ esp32.ino
@@ -0,0 +1,56 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#define BUTTON_PIN 1
+
+const char *ssid = "{ssid}";
+const char *password = "{password}";
+
+const String token = "{token}";
+
+
+void setup() {
+  WiFi.begin(ssid, password);
+
+  Serial.begin(115200);
+  Serial.print("Connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Connected to Wi-Fi");
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+  static int lastState = LOW;
+
+  int state = digitalRead(BUTTON_PIN);
+  if (state == LOW && lastState == HIGH) {
+    sendStatus("TRUE");
+  }
+  if (state == HIGH && lastState == LOW) {
+    sendStatus("FALSE");
+  }
+  lastState = state;
+  delay(100);
+}
+
+void sendStatus(String message) { 
+  HTTPClient client;
+
+  String url = "https://api.statuspage.io/v1/pages/" + pageId + "/components/" + componentId ;
+  client.begin(url);
+  client.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  client.addHeader("Authorization", "Bearer " + token);
+
+  String query = "message=" + message;
+  client.POST(query);
+
+  String body = client.getString();
+  Serial.println("Sent the message");
+  Serial.println(body);
+  client.end();
+}
